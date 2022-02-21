0f45d0f45e9cfd00995cfbbb4cff22da8837a2c5
vincent01110 <bencekristof06@gmail.com>
2022-02-21 09:12:46 +0100

    upload ch14

--- ch14/main.cpp
+++ ch14/main.cpp
@@ -1,61 +1,63 @@
-#include "Graph.h"
-#include "Simple_window.h"
+/* g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images` */
+#include "std_lib_facilities.h"
+
+struct B1 {
+  virtual void vf() {cout << "B1::vf()" << endl;};
+  void f() {cout << "B1::f()" << endl;};
+  virtual void pvf() =0;
+};
+
+struct D1:B1 {
+  void vf() override{cout << "D1:B1::vf()" << endl;};
+  void f() {cout << "D1:B1::f()" << endl;};
+};
+
+struct D2:D1 {
+  void pvf() override {cout << "D2:D1::pvf()" << endl;};
+};
+
+struct B2 {
+  virtual void pvf() =0;
+};
+
+struct D21:B2 {
+  string str = "D21:B2::pvf()";
+  void pvf() override {cout << str << endl;};
+};
+
+struct D22:B2 {
+  int i = 30;
+  void pvf() override {cout << i << endl;};
+};
+
+void f(B2& b) {
+  b.pvf();
+}
 
 int main() {
 
-  using namespace Graph_lib;
+  /*B1 b;
+  b.vf();
+  b.f();
 
-  Simple_window win{Point{100,100}, 800, 1000, "Ch13"};
+  D1 d;
+  d.vf();
+  d.f();
 
-  Lines grid;
-  int x_size = win.x_max();
-  int y_size = win.y_max();
+  B1& ref {d};
+  ref.vf();
+  ref.f();
 
-  for (int x = 100; x < x_size; x+=100) {
-    grid.add(Point{x,0}, Point{x,y_size});
-  }
+  D2 d2;
+  d2.vf();
+  d2.f();
+  d2.pvf();*/
 
-  for (int y = 100; y < y_size; y+=100) {
-    grid.add(Point{0,y}, Point{x_size,y});
-  }
-  win.attach(grid);
+  D21 d_21;
+  f(d_21);
 
-  Vector_ref<Rectangle> vr;
-  for (int i = 0; i <= x_size-100; i+=100) {
-    vr.push_back(new Rectangle {Point{i,i}, 100,100});
-    vr[vr.size()-1].set_color(Color::red);
-    vr[vr.size()-1].set_style(Line_style(Line_style::solid, 4));
-    win.attach(vr[vr.size()-1]);
-  }
+  D22 d_22;
+  f(d_22);
 
-  /*Vector_ref<Image> iv;
-  for (i = 0; i <= x_size-100; i+=100) {
-    iv.push_back(new Image{Point{i,i}, "cat.jpg"});
-    win.attach(iv[iv.size()-1]);
-  }*/
-
-  Image img{Point{0,400}, "cat.jpg"};
-  img.set_mask(Point{20,100}, 200, 200);
-  Image img2{Point{400,0}, "cat.jpg"};
-  img2.set_mask(Point{20,100}, 200, 200);
-  Image img3{Point{300,700}, "cat.jpg"};
-  img3.set_mask(Point{20,100}, 200, 200);
-  win.attach(img);
-  win.attach(img2);
-  win.attach(img3);
-
-  int i,j;
-  Image img_gru{Point{0,0}, "gru_re.jpg"};
-  win.attach(img_gru);
-  for (i = 0; i <= y_size-100; i+=100) {
-    for (j = 0; j <= x_size-100; j+=100) {
-      win.wait_for_button();
-      img_gru.move(100, 0);
-      win.attach(img_gru);
-    }
-    img_gru.move(-x_size, 100);
-  }
-
-  win.wait_for_button();
 
 }
