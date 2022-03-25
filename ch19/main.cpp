10806ea3fc68139aa38f7381c00bda44174d4911
vincent01110 <bencekristof06@gmail.com>
2022-03-25 17:54:42 +0100

    ch16_final

---
+++ ch19/main.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main()
+try{
+  vector<double> vd;
+  for(double d; cin >> d;){
+    vd.push_back(vd);
+  }
+
+
+}
+catch (exception& e){
+  cerr << e.what() << endl;
+  return 1;
+}
+catch (...){
+  cerr << "Unknown exception" << endl;
+  return 2;
+}
++
