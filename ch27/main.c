f54b597a45f914d118bac7f21e86b8ed8c2c1ac2
vincent01110 <bencekristof06@gmail.com>
2022-04-26 08:29:15 +0200

    idk

---
+++ ch27/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_out(const char* p, const int x)
+{
+    printf("p is\"%s\" and x is %i\n", p, x);
+}
+
+int main()
+{
+    printf("Hello World!\n");
+
+    char* hello = "Hello";
+    char* world = "World!";
+    printf("%s %s\n", first, last);
+
+    print_out("May", 6);
+    print_out("Chris", 39);
+    print_out("C", 89);
+    return 0;
+}
